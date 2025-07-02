93c495822d9ddf8ee66fedfb0d607e6b306c1497
Guillaume <guillaume.thouvenin@vates.tech>
2025-07-02 20:39:25 +0200

    Read path

--- lib/server.ml
+++ lib/server.ml
@@ -12,15 +12,25 @@ let server_handler _conn req _body =
   let open Cohttp_lwt_unix in
   let met = req |> Request.meth |> Cohttp.Code.string_of_method in
   let hdr_lst = req |> Request.headers |> Cohttp.Header.to_list in
+  let path = req |> Request.resource in
 
-  (* Print some infos *)
-  print_endline ("Method is " ^ met);
-  print_endline "Header list: ";
-  List.iter (fun (s1, s2) -> print_endline ("  " ^ s1 ^ " -> " ^ s2)) hdr_lst;
+  (* Print some infos about the request *)
+  Printf.printf "Method is %s\n" met;
+  Printf.printf "Path is %s\n" path;
+  Printf.printf "Header list:\n";
+  List.iter (fun (s1, s2) -> Printf.printf "  %s -> %s\n" s1 s2) hdr_lst;
+  flush stdout;
 
   (* and return OK *)
-  let hello = `String "Hello, Sailor!" in
-  Cohttp_lwt_unix.Server.respond ~status:`OK ~body:hello ()
+  let body =
+    match String.lowercase_ascii path with
+    | "/joe" ->
+        (* We want to keep upper/lower case from path so extract it. *)
+        let name = String.sub path 1 3 in
+        `String (Printf.sprintf "Hello %s!" name)
+    | _ -> `String "Hello, Sailor!"
+  in
+  Cohttp_lwt_unix.Server.respond ~status:`OK ~body ()
 
 let start_server =
   let server =
