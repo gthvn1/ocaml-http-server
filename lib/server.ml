7e11c8d088ad65f7b916e7c1b5438d1501bac5ee
Guillaume <guillaume.thouvenin@vates.tech>
2025-07-03 19:17:06 +0200

    Add textarea to act as the repl of future lox

--- lib/server.ml
+++ lib/server.ml
@@ -22,32 +22,36 @@ let read_file name : string =
   let sz = in_channel_length in_file in
   really_input_string in_file sz
 
-let server_handler _conn req _body =
+let server_handler _conn req body =
   let open Cohttp_lwt_unix in
+  let ( >>= ) = Lwt.bind in
   let met = req |> Request.meth |> Cohttp.Code.string_of_method in
   let hdr_lst = req |> Request.headers |> Cohttp.Header.to_list in
   let path = req |> Request.resource in
 
+  Cohttp_lwt.Body.to_string body >>= fun body_str ->
   (* Print some infos about the request *)
-  Printf.printf "Method is %s\n" met;
-  Printf.printf "Path is %s\n" path;
-  Printf.printf "Header list:\n";
-  List.iter (fun (s1, s2) -> Printf.printf "  %s -> %s\n" s1 s2) hdr_lst;
+  Printf.printf "- Method: %s\n" met;
+  Printf.printf "- Path: %s\n" path;
+  Printf.printf "== Header list\n";
+  List.iter (fun (s1, s2) -> Printf.printf "  - %s -> %s\n" s1 s2) hdr_lst;
+  Printf.printf "== Body\n%s\n\n" body_str;
   flush stdout;
 
   (* and return OK *)
-  let body =
+  let answer =
     match String.lowercase_ascii path with
     | "/joe" ->
         (* We want to keep upper/lower case from path so extract it. *)
         let name = String.sub path 1 3 in
         `String (Printf.sprintf "Hello, %s!" name)
     | "/clicked" -> `String "Whoa, you actually clicked it!"
+    | "/lox" -> `String "Not yet implemented"
     | "/favicon.ico" -> `String (read_file "favicon")
     | "/style.css" -> `String (read_file "css")
     | _ -> `String (read_file "_index")
   in
-  Cohttp_lwt_unix.Server.respond ~status:`OK ~body ()
+  Cohttp_lwt_unix.Server.respond ~status:`OK ~body:answer ()
 
 let start_server =
   let server =
